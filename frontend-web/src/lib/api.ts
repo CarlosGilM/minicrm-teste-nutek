cd7da1b1740b38975b25b5be401a5af1b3d3b265
Carlos Gil <carlosgilmds@gmail.com>
2026-03-18 21:49:16 -0300

    fix bug login frontend

--- frontend-web/src/lib/api.ts
+++ frontend-web/src/lib/api.ts
@@ -33,7 +33,7 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if (error.response?.status === 401 && !originalRequest._retry && !originalRequest.url?.includes('/auth/')) {
       originalRequest._retry = true;
 
       try {
